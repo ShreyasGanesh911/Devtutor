d8f00ce2a164acf3d2816935010df3044fff850a
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2025-05-23 23:04:00 +0530

    defined transcript endpoint

--- app/api/session/route.ts
+++ app/api/session/route.ts
@@ -1,5 +1,46 @@
 import { NextResponse } from "next/server";
-
+const apiKey = process.env.OPENAI_API_KEY
 export async function GET(request:Request){
-   return NextResponse.json({message:"Hello from the API!"})
+   const URL = "https://api.openai.com/v1/realtime/sessions"
+  
+   try {
+      const response = await fetch(URL,{
+         method:"POST",
+         headers:{
+            "Content-Type":"application/json",
+            "Authorization":`Bearer ${apiKey}`
+         },
+         body:JSON.stringify({
+            model: "gpt-4o-realtime-preview-2024-12-17",
+            voice: "sage",
+         })
+      })
+      const data = await response.json()
+      return NextResponse.json(data,{status:200})
+   } catch (error) {
+      return NextResponse.json({message:"Error creating session",error},
+         {status:500}
+      )
+   }
+   
+}
+
+export async function POST(request:Request){
+   try{
+      const formData = await request.formData()
+   const response = await fetch("https://api.openai.com/v1/audio/transcriptions",{
+      method:"POST",
+      headers:{
+         // "Content-Type":"multipart/form-data",
+         "Authorization":`Bearer ${apiKey}`
+      },
+      body:formData
+   })
+   const data = await response.json()
+   return NextResponse.json(data,{status:200})
+   }catch(e){
+      return NextResponse.json({message:"Error creating session",e},
+         {status:500}
+      )
+   }
 }
