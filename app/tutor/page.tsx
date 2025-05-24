13803f43626ec786b152b1a837199172db4cf81b
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2025-05-24 19:56:18 +0530

    added editor component

--- app/tutor/page.tsx
+++ app/tutor/page.tsx
@@ -1,12 +1,14 @@
+'use client'
 import React from 'react'
 import Chat from './Chat'
+import Editor from './codeEditor/Editor'
 function page() {
   return (
-    <div className='flex w-full h-full'>
+    <div className='flex w-full h-full bg-gray-800'>
       
       <Chat/>
       <div className='w-1/2'>
-        <h1>Tutor</h1>
+        <Editor/>
       </div>
     </div>
   )
